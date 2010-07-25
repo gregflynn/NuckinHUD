fb3d3cce89bc5e6b61690c2bcd63d8c1dd0a6eea
Greg Flynn <gflynn1@binghamton.edu>
2010-07-25 12:11:22 -0400

    basic lua file structure

---
+++ NuckinHUD.lua
@@ -0,0 +1,22 @@
+NuckinHUD_Modules = {}
+
+function NuckinHUD_OnLoad(self)
+	--core OnLoad
+	
+	--modules OnLoad
+	
+end
+
+function NuckinHUD_OnEvent(self, event, arg1, arg2)
+	--core OnEvent
+	
+	--modules OnEvent
+	
+end
+
+function NuckinHUD_OnUpdate(self, elapsed)
+	--core OnUpdate
+	
+	--modules OnUpdate
+	
+end
