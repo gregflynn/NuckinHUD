b5f75d205e8a1ab9324b360ca60d0956cec0d65b
Greg Flynn <gflynn1@binghamton.edu>
2010-07-25 22:58:42 -0400

    basic module file structure, PlayerHP's OnLoad is not being called yet though:

---
+++ Modules/PlayerHP.lua
@@ -0,0 +1,5 @@
+_G["NuckinHUD_Modules"]["PlayerHP"] = true
+
+function NuckinHUD_PlayerHP_OnLoad(self)
+	print("Player HP Loaded")
+end
--- NuckinHUD.lua
+++ NuckinHUD.lua
@@ -2,9 +2,13 @@ NuckinHUD_Modules = {}
 
 function NuckinHUD_OnLoad(self)
 	--core OnLoad
-	
+	print("NuckinHUD Loaded")
 	--modules OnLoad
-	
+	for key,value in pairs(NuckinHUD_Modules) do
+		if value then
+			_G["NuckinHUD_"..key.."_OnLoad"](self)
+		end
+	end
 end
 
 function NuckinHUD_OnEvent(self, event, arg1, arg2)
