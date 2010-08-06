79f68306f276d9ae6f18ff1ab4d0f9393edac45c
Greg Flynn <gflynn1@binghamton.edu>
2010-08-05 22:39:39 -0400

    added some comments

--- NuckinHUD.lua
+++ NuckinHUD.lua
@@ -43,6 +43,7 @@ function NuckinHUD:OnUpdate(self, elapsed)
 	end
 end
 
+--This function registers a module with the library
 function NuckinHUD:AddModule(name)
     if NuckinHUD_Modules[name] then
         NuckinHUD:error("Duplicate module loaded named '"..name.."'")
@@ -51,10 +52,16 @@ function NuckinHUD:AddModule(name)
     end
 end
 
+--This function registers a graphic with the core
 function NuckinHUD:AddGraphic(name)
     if NuckinHUD_Graphics[name] then
         NuckinHUD:error("Duplicate graphics library loaded named '"..name..."'")
     else
         NuckinHUD_Graphics[name] = true
     end
+end
+
+--This function registers an event with the core
+function NuckinHUD:AddEvent(event)
+    _G["NuckinHUD"]:RegisterEvent(event)
 end
